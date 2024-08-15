cb53b4120cc226324198b6f7758e8eb3ad190a67
yzakirfakruddin <zakiryadiki@gmail.com>
2024-08-16 04:28:32 +0530

    project complete

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const enteredPlayerName = useRef();
+  const [playerName, setPlayerName] = useState(null);
+
+  const handleClick = () => {
+    setPlayerName(enteredPlayerName.current.value);
+    enteredPlayerName.current.value = null;
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ? playerName : "Guest"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={enteredPlayerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
