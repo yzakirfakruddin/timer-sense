06aa30c514dfcad72b57fe0be795a75af0c7bdf3
yzakirfakruddin <zakiryadiki@gmail.com>
2024-08-16 04:38:43 +0530

    add portal for ResultModal to finally complete project

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom"; //using this createPortal only to move this from the deep-inside the html to close to root or the starting of the body.
 
 const ResultModal = forwardRef(function ResultModal(
   { targetTime, onReset, remainingTime },
@@ -17,7 +18,7 @@ const ResultModal = forwardRef(function ResultModal(
       },
     };
   });
-  return (
+  return createPortal(
     <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost && <h2>You Lost</h2>}
       {!userLost && <h2>Your Score: {score}</h2>}
@@ -31,7 +32,8 @@ const ResultModal = forwardRef(function ResultModal(
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 });
 
